629bcad13b5434ae5681730daf24eff2f4d2e1b7
Art.Sled <art.sled.ua@gmail.com>
2018-11-13 00:20:59 +0300

    13/11/2018

--- output.php
+++ output.php
@@ -10,3 +10,22 @@
 	<?php echo $output ?>
 </body>
 </html>
+<?php
+
+		try{
+			$sql = 'CREATE TABLE joke(
+				id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+				joketext TEXT,
+				jpkedata DATA NOT NULL
+			) DEFAULT CHARACTER SET utf8 ENGINE=dbroot';
+			$pdo->exec($sql);
+		}
+		catch(PDOException $e){
+		$output = 'Ошибка при создании таблицы joke: ' . $e->getMessage();
+		include 'output.php';
+		exit();
+		}
+		
+		$output = 'Таблица joke была успешно создана.';
+		include 'output.php';
+		?>
