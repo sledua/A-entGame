23f883f6fa7ec9d8ce2f8f669308b759b6cc2056
Artem <art.sled.ua@gmail.com>
2019-08-03 08:23:57 +0300

    clear

--- app/name.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$output = '';
-for ($countfo = 2; $countfo <= 10; ++$countfo){
-	$output .= "$countfo ";
-}
-include 'name.html';
--- test.js
+++
@@ -1,6 +0,0 @@
-var a =6;
-var b =4;
-var x ;
-x=((a*b)+(a%b))*2;
-document.write(a,b)
-document.write(x)
